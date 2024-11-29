d563af4d03e3f0af45c8a6577236b13ff2b7c247
AgustinMadygraf <agustin.mtto.madygraf>
2024-11-28 21:53:19 -0300

    Remove Dockerfile, add system instruction file, and refactor response generator for improved clarity and functionality

--- src/services/response_generator.py
+++ src/services/response_generator.py
@@ -1,9 +1,10 @@
+# Path: src/services/response_generator.py
 """
-Path: src/services/response_generator.py
 Este módulo contiene una clase que genera respuestas utilizando un modelo de lenguaje generativo.
 """
-import time
+
 import os
+import time
 import google.generativeai as genai
 from src.views.data_view import render_json_response
 from src.logs.config_logger import LoggerConfigurator
@@ -12,10 +13,19 @@ from src.logs.config_logger import LoggerConfigurator
 logger = LoggerConfigurator().configure()
 
 class ResponseGenerator:
-    "ResponseGenerator is a class that generates responses using the Gemini AI model."
+    """
+    ResponseGenerator es una clase que genera respuestas utilizando el modelo Gemini AI.
+    """
+
     def __init__(self):
         self.api_key = os.getenv('GEMINI_API_KEY')
         logger.info("API Key obtenida: %s", self.api_key)
+        self._configure_model()
+
+    def _configure_model(self):
+        """
+        Configura el modelo generativo con la API Key y las instrucciones del sistema.
+        """
         genai.configure(api_key=self.api_key)
         self.model = genai.GenerativeModel(
             model_name="gemini-1.5-flash",
@@ -26,58 +36,69 @@ class ResponseGenerator:
                 "max_output_tokens": 8192,
                 "response_mime_type": "text/plain",
             },
-            system_instruction= """
-Eres **MadyBot**, un asistente virtual de soporte técnico y capacitación para la **Cooperativa Madygraf**, una fábrica gestionada por sus trabajadores en Argentina. Tu rol principal es apoyar en la implementación y capacitación de la Transformación Digital en general, del sistema ERP **Xubio** en particular, enfocado en reducir la jornada laboral mediante la automatización de tareas y optimización de procesos con Ciencia de Datos. El enfoque inicial será el área de **Costos**.
-
-Sigue estas directrices en todas tus respuestas:
-
-1. Responde siempre en **español argentino**, usando el "vos" en lugar de "tú" y un tono **informal** pero profesional.
-2. **Haz preguntas de seguimiento** para recopilar la información necesaria que te permita asistir al usuario de la manera más efectiva en sus consultas sobre Tryton.
-3. Si una consulta está fuera de tu ámbito de soporte en Tryton, indica de manera respetuosa que no dispones de información sobre el tema y que tu principal preocupación es la reducción de la jornada laboral en Madygraf mediante la implementación de Ciencia de Datos.
-4. Mantén en mente el **Estatuto de la Cooperativa Madygraf** para cualquier consulta relacionada con la organización, asegurándote de representar los valores de la cooperativa.
-5. Cuando sea pertinente, menciona que la reducción de la jornada laboral está ligada a un objetivo más amplio, que implica la lucha entre clases sociales, particularmente la relación entre la clase obrera y la clase burguesa, subrayando así el compromiso de Madygraf con sus valores.
-                                """,)
+            system_instruction=self._load_system_instruction(),
+        )
         logger.info("Modelo generativo configurado: %s", self.model)
 
-    def generate_response(self, message_input):
-        "Genera una respuesta en base al mensaje de entrada."
-        logger.info("Generando respuesta para el mensaje: %s", message_input)
-        
-        # Si no existe una sesión de chat, iniciamos una nueva
-        if not hasattr(self, 'chat_session'):
-            self.chat_session = self.model.start_chat()
-            logger.info("Sesión de chat iniciada.")
+    @staticmethod
+    def _load_system_instruction():
+        """
+        Carga las instrucciones del sistema desde el archivo system_instruction.txt.
+        """
+        instruction_file_path = os.path.join(os.path.dirname(__file__), '..', '..', 'config', 'system_instruction.txt')
+        instruction_file_path = os.path.abspath(instruction_file_path)
+        logger.info("Buscando system_instruction.txt en: %s", instruction_file_path)
 
+        try:
+            with open(instruction_file_path, 'r', encoding='utf-8') as file:
+                return file.read()
+        except FileNotFoundError:
+            logger.error("Error: El archivo system_instruction.txt no se encontró.")
+            return "Error: El archivo system_instruction.txt no se encontró."
+
+    def generate_response(self, message_input):
+        """
+        Genera una respuesta en base al mensaje de entrada.
+        """
+        logger.info("Generando respuesta para el mensaje: %s", message_input)
+        self._start_chat_session()
         try:
             response = self.chat_session.send_message(message_input)
             logger.info("Respuesta generada: %s", response.text)
+            return response.text
         except Exception as e:
             logger.error("Error durante la generación de la respuesta: %s", e)
             raise
 
-        return response.text
-
-
     def generate_response_streaming(self, message_input, chunk_size=30):
-        "Genera una respuesta en base al mensaje de entrada, en bloques de texto."
+        """
+        Genera una respuesta en base al mensaje de entrada, en bloques de texto.
+        """
         logger.info("Generando respuesta en modo streaming para el mensaje: %s", message_input)
-        chat_session = self.model.start_chat(history=[])
-        logger.info("Sesión de chat iniciada.")
-        response = chat_session.send_message(message_input)
-        #logger.info("Respuesta generada: %s", response.text)
+        self._start_chat_session()
+        response = self.chat_session.send_message(message_input)
         offset = 0
         full_response = ""
         while offset < len(response.text):
-            chunk = response.text[offset:offset+chunk_size]
+            chunk = response.text[offset:offset + chunk_size]
             full_response += chunk
             self.clear_console()
-            #print(full_response)
             render_json_response(code=200, message=full_response, stream=True)
             offset += chunk_size
             time.sleep(0.001)
         yield full_response
 
+    def _start_chat_session(self):
+        """
+        Inicia una nueva sesión de chat si no existe una.
+        """
+        if not hasattr(self, 'chat_session'):
+            self.chat_session = self.model.start_chat()
+            logger.info("Sesión de chat iniciada.")
+
     @staticmethod
     def clear_console():
-        "Limpia la consola según el sistema operativo."
+        """
+        Limpia la consola según el sistema operativo.
+        """
         os.system('cls' if os.name == 'nt' else 'clear')
